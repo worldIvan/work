ceec2cbb04cabd2b09882f7a4c27ecc3b2d2e9ea
worldIvan <ivan.sribnuy@gmail.com>
2024-11-26 15:08:00 +0200

    Add files via upload

---
+++ laba62.cpp
@@ -0,0 +1,26 @@
+﻿//Листинг 2 ‐ Вычисление функции e^x по разложению в ряд Маклорена
+
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "file. f0001_2" << endl;
+	double x = 1;
+	double a[2] = { 1, 0 };
+	double Sa = a[0];
+	
+	double eps = 1;
+
+	int n = 1;
+
+	while (eps > 0.001)
+	{
+		eps = Sa;
+		a[1] = a[0] * x / n;
+		Sa += a[1];
+		eps = Sa - eps;
+		a[0] = a[1];
+		n++;
+	}
+	cout << "e^x = f(x=1) = " << Sa << endl;
+	return 0;
+}
